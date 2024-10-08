4556796769f370ced97509acfe04678936b6156b
tian <istiancz@gmail.com>
2024-10-08 20:06:20 +0800

    update

--- src/App.tsx
+++ src/App.tsx
@@ -52,7 +52,7 @@ function App() {
 		<main className='w-screen h-screen'>
 			<iframe
 				className='w-screen h-full'
-				src='https://cn.bing.com/search?q=dff'
+				src='https://cn.bing.com/search?q=产品经理职责'
 			/>
 			<div className='opacity-0 hover:opacity-100 transition-all fixed bottom-0 bg-white flex w-screen p-2 pl-6 bg-transparent items-center gap-2 overflow-y-scroll flex-wrap'>
 				{stockList.sh?.map((stock) => (
