7f49a135f548af54e79122f44056cf22a01414a2
tian <istiancz@gmail.com>
2024-10-09 10:41:28 +0800

    sw test

--- sw.js
+++ sw.js
@@ -1,21 +1,35 @@
 console.log('sw')
 if ('serviceWorker' in navigator) {
-	window.addEventListener('load', function () {
-		navigator.serviceWorker.register('./sw-proxy.js')
+	window.addEventListener('load', async function () {
+		try {
+			const registration = await navigator.serviceWorker.register('/sw.js', {
+				scope: '/',
+			})
+			if (registration.installing) {
+				console.log('正在安装 Service worker')
+			} else if (registration.waiting) {
+				console.log('已安装 Service worker installed')
+			} else if (registration.active) {
+				console.log('激活 Service worker')
+			}
 
-		console.log('sw')
-		// 2.拦截请求（sw-proxy.js）
-		self.addEventListener('fetch', async (event) => {
-			console.log('fetch', event)
-			const { request } = event
-			let response = await fetch(request)
-			// 3.重新构造Response
-			response = new Response(response.body, response)
-			// 4.篡改响应头
-			response.headers.delete('Content-Security-Policy')
-			response.headers.delete('X-Frame-Options')
+			console.log('sw')
+			// 2.拦截请求（sw-proxy.js）
+			self.addEventListener('fetch', async (event) => {
+				event.respondWith(Promise.resolve({}))
+				console.log('onfetch', event)
+				const { request } = event
+				let response = await fetch(request)
+				// 3.重新构造Response
+				response = new Response({ ...response.body, hi: 1 }, response)
+				// 4.篡改响应头
+				response.headers.delete('Content-Security-Policy')
+				response.headers.delete('X-Frame-Options')
 
-			event.respondWith(Promise.resolve(originalResponse))
-		})
+				event.respondWith(Promise.resolve(response))
+			})
+		} catch (error) {
+			console.error(`注册失败：${error}`)
+		}
 	})
 }
