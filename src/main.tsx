1dc5bae099430b55bf1fab540b9dfa992adf9e4e
tian <istiancz@gmail.com>
2024-10-09 17:13:12 +0800

    update

--- src/main.tsx
+++ src/main.tsx
@@ -6,14 +6,6 @@ import { inject } from '@vercel/analytics'
 
 inject()
 
-if ('serviceWorker' in navigator) {
-	console.log('serviceWorker in navigator')
-	navigator.serviceWorker.getRegistrations().then((registrations) => {
-		for (const registration of registrations) {
-			registration.unregister()
-		}
-	})
-}
 createRoot(document.getElementById('root')!).render(
 	<StrictMode>
 		<App />
