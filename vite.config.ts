4bf2e46152063b74e01900da86f0b21d94d3cb9a
tian <istiancz@gmail.com>
2024-10-09 16:38:42 +0800

    fix sw

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,13 @@ import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), VitePWA({})],
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			workbox: {
+				globPatterns: ['**/*.{ico,png,svg}'],
+			},
+		}),
+	],
 })
