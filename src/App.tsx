a29f4b7f1db2be3a9fd74e406db99cedb8102aa7
tian <istiancz@gmail.com>
2024-10-10 14:19:01 +0800

    feat: add hk

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,19 @@
 import { useCallback, useEffect, useRef, useState } from 'react'
 import './App.css'
-import { getShValue, getSzValue, StockValue } from './utils/api'
+import { getHkValue, getShValue, Market, Stock, getSzValue } from './utils/api'
 import { useAtom } from 'jotai'
 import { atomWithStorage } from 'jotai/utils'
 import clsx from 'clsx'
 
-const defaultCodeList: { type: 'sh' | 'sz'; code: string }[] = [
+const defaultCodeList: { type: Market; code: string }[] = [
 	{ type: 'sh', code: '000001' },
 	{ type: 'sh', code: '399001' },
 	{ type: 'sh', code: '399006' },
 	{ type: 'sh', code: '600519' },
 	{ type: 'sh', code: '510300' },
+	{ type: 'hk', code: '00700' },
 ]
-const codeListAtom = atomWithStorage<{ type: 'sh' | 'sz'; code: string }[]>(
+const codeListAtom = atomWithStorage<{ type: Market; code: string }[]>(
 	'codeList',
 	defaultCodeList
 )
@@ -24,7 +25,7 @@ const fontSizeAtom = atomWithStorage<'xs' | 'sm' | 'base' | 'xl'>(
 
 const showSettingAtom = atomWithStorage<boolean>('showSetting', true)
 function App() {
-	const [stockList, setStockList] = useState<StockValue[]>([])
+	const [stockList, setStockList] = useState<Stock[]>([])
 	const [codeList, setCodeList] = useAtom(codeListAtom)
 	const [fontSize, setFontSize] = useAtom(fontSizeAtom)
 	const [showName, setShowName] = useAtom(showNameAtom)
@@ -36,25 +37,27 @@ function App() {
 			setCodeList([
 				...sh.map((c) => ({ type: 'sh', code: c })),
 				...sz.map((c) => ({ type: 'sz' as const, code: c })),
-			] as { type: 'sh' | 'sz'; code: string }[])
+			] as { type: Market; code: string }[])
 		}
 	}, [codeList, setCodeList])
 
 	const fetchStock = useCallback(async () => {
 		const shCodes = codeList.filter((c) => c.type === 'sh').map((c) => c.code)
 		const szCodes = codeList.filter((c) => c.type === 'sz').map((c) => c.code)
+		const hkCodes = codeList.filter((c) => c.type === 'hk').map((c) => c.code)
 
-		const [sh, sz] = await Promise.all([
+		const [sh, sz, hk] = await Promise.all([
 			getShValue(shCodes),
 			getSzValue(szCodes),
+			getHkValue(hkCodes),
 		])
 
 		const orderedStocks = codeList
 			.map(({ type, code }) => {
-				const stockList = type === 'sh' ? sh : sz
-				return stockList.find((stock) => stock.f12 === code)
+				const stockList = type === 'sh' ? sh : type === 'sz' ? sz : hk
+				return stockList.find((stock) => stock.code === code)
 			})
-			.filter(Boolean) as StockValue[]
+			.filter(Boolean) as Stock[]
 		setStockList(orderedStocks)
 	}, [codeList])
 
@@ -63,8 +66,8 @@ function App() {
 	}, [fetchStock])
 
 	const [pendingStock, setPendingStock] = useState<{
-		sh: StockValue | null
-		sz: StockValue | null
+		sh: Stock | null
+		sz: Stock | null
 	}>({
 		sh: null,
 		sz: null,
@@ -73,12 +76,16 @@ function App() {
 		e.preventDefault()
 		const target = e.target as HTMLFormElement
 		const code = (target.elements[0] as HTMLInputElement).value
+		const hk = (await getHkValue([code]))?.[0]
 		const sh = (await getShValue([code]))?.[0]
 		const sz = (await getSzValue([code]))?.[0]
 
-		let type: 'sh' | 'sz' | null = null
+		console.log('hk', hk)
+		let type: Market | null = null
 
-		if (
+		if (code.length === 5 && hk) {
+			type = 'hk'
+		} else if (
 			codeList.some((c) => c.code === code && c.type === 'sh') &&
 			codeList.some((c) => c.code === code && c.type === 'sz')
 		) {
@@ -160,7 +167,7 @@ function App() {
 										...codeList,
 										{
 											type: 'sh' as const,
-											code: pendingStock.sh!.f12,
+											code: pendingStock.sh!.code,
 										},
 									])
 									setPendingStock({
@@ -169,7 +176,7 @@ function App() {
 									})
 								}}
 							>
-								{pendingStock.sh!.f14}
+								{pendingStock.sh!.name}
 							</button>
 						)}
 						{pendingStock.sz && (
@@ -180,7 +187,7 @@ function App() {
 										...codeList,
 										{
 											type: 'sz' as const,
-											code: pendingStock.sz!.f12,
+											code: pendingStock.sz!.code,
 										},
 									])
 									setPendingStock({
@@ -189,7 +196,7 @@ function App() {
 									})
 								}}
 							>
-								{pendingStock.sz!.f14}
+								{pendingStock.sz!.name}
 							</button>
 						)}
 						<div className='flex items-center gap-2 border rounded px-2'>
@@ -252,30 +259,24 @@ const useInterval = (callback: IntervalFunction, delay: number | null) => {
 }
 
 const showNameAtom = atomWithStorage<boolean>('showName', true)
-const StockItem = ({
-	stock,
-	type,
-}: {
-	stock: StockValue
-	type: 'sh' | 'sz'
-}) => {
+const StockItem = ({ stock, type }: { stock: Stock; type: Market }) => {
 	const [, setCodeList] = useAtom(codeListAtom)
 	const [showName, setShowName] = useAtom(showNameAtom)
 	return (
 		<div
 			className='relative group bg-transparent px-2 py-1 rounded transition-all flex-shrink-0'
-			key={stock.f14}
+			key={stock.name}
 		>
 			<div className='opacity-0 group-hover:opacity-100 transition-all absolute -top-2 right-0 flex gap-1'>
 				<button
 					onClick={() => {
 						setCodeList((c) => {
 							const index = c.findIndex(
-								(c) => c.type === type && c.code === stock.f12
+								(c) => c.type === type && c.code === stock.code
 							)
 							if (index === -1) return c
 							const newCodeList = [...c.slice(0, index), ...c.slice(index + 1)]
-							newCodeList.unshift({ type, code: stock.f12 })
+							newCodeList.unshift({ type, code: stock.code })
 							return newCodeList
 						})
 					}}
@@ -286,7 +287,7 @@ const StockItem = ({
 				<button
 					onClick={() => {
 						setCodeList((c) =>
-							c.filter((c) => !(c.type === type && c.code === stock.f12))
+							c.filter((c) => !(c.type === type && c.code === stock.code))
 						)
 					}}
 					className='border p-0.5 rounded bg-transparent hover:bg-red-100'
@@ -295,13 +296,11 @@ const StockItem = ({
 				</button>
 			</div>
 			<span className='cursor-pointer' onClick={() => setShowName(!showName)}>
-				{showName ? stock.f14 : stock.f12}
+				{showName ? stock.name : stock.code}
 			</span>
-			<span className='font-mono'> {(stock.f2 / 100).toFixed(2)}</span>
-			<span>{stock.f4 >= 0 ? '△' : '▽'}</span>
-			<span className='font-mono'>
-				{((stock.f4 * 100) / stock.f18).toFixed(2)}%
-			</span>
+			<span className='font-mono'> {stock.current}</span>
+			<span>{stock.percent >= 0 ? '△' : '▽'}</span>
+			<span className='font-mono'>{stock.percent.toFixed(2)}%</span>
 		</div>
 	)
 }
@@ -317,8 +316,8 @@ const EyeOpen = () => (
 		<path
 			d='M7.5 11C4.80285 11 2.52952 9.62184 1.09622 7.50001C2.52952 5.37816 4.80285 4 7.5 4C10.1971 4 12.4705 5.37816 13.9038 7.50001C12.4705 9.62183 10.1971 11 7.5 11ZM7.5 3C4.30786 3 1.65639 4.70638 0.0760002 7.23501C-0.0253338 7.39715 -0.0253334 7.60288 0.0760014 7.76501C1.65639 10.2936 4.30786 12 7.5 12C10.6921 12 13.3436 10.2936 14.924 7.76501C15.0253 7.60288 15.0253 7.39715 14.924 7.23501C13.3436 4.70638 10.6921 3 7.5 3ZM7.5 9.5C8.60457 9.5 9.5 8.60457 9.5 7.5C9.5 6.39543 8.60457 5.5 7.5 5.5C6.39543 5.5 5.5 6.39543 5.5 7.5C5.5 8.60457 6.39543 9.5 7.5 9.5Z'
 			fill='currentColor'
-			fill-rule='evenodd'
-			clip-rule='evenodd'
+			fillRule='evenodd'
+			clipRule='evenodd'
 		></path>
 	</svg>
 )
@@ -334,8 +333,8 @@ const EyeClose = () => (
 		<path
 			d='M14.7649 6.07596C14.9991 6.22231 15.0703 6.53079 14.9239 6.76495C14.4849 7.46743 13.9632 8.10645 13.3702 8.66305L14.5712 9.86406C14.7664 10.0593 14.7664 10.3759 14.5712 10.5712C14.3759 10.7664 14.0593 10.7664 13.8641 10.5712L12.6011 9.30817C11.805 9.90283 10.9089 10.3621 9.93375 10.651L10.383 12.3277C10.4544 12.5944 10.2961 12.8685 10.0294 12.94C9.76267 13.0115 9.4885 12.8532 9.41704 12.5865L8.95917 10.8775C8.48743 10.958 8.00036 10.9999 7.50001 10.9999C6.99965 10.9999 6.51257 10.958 6.04082 10.8775L5.58299 12.5864C5.51153 12.8532 5.23737 13.0115 4.97064 12.94C4.7039 12.8686 4.5456 12.5944 4.61706 12.3277L5.06625 10.651C4.09111 10.3621 3.19503 9.90282 2.3989 9.30815L1.1359 10.5712C0.940638 10.7664 0.624058 10.7664 0.428798 10.5712C0.233537 10.3759 0.233537 10.0593 0.428798 9.86405L1.62982 8.66303C1.03682 8.10643 0.515113 7.46742 0.0760677 6.76495C-0.0702867 6.53079 0.000898544 6.22231 0.235065 6.07596C0.469231 5.9296 0.777703 6.00079 0.924058 6.23496C1.40354 7.00213 1.989 7.68057 2.66233 8.2427C2.67315 8.25096 2.6837 8.25972 2.69397 8.26898C4.00897 9.35527 5.65537 9.99991 7.50001 9.99991C10.3078 9.99991 12.6564 8.5063 14.076 6.23495C14.2223 6.00079 14.5308 5.9296 14.7649 6.07596Z'
 			fill='currentColor'
-			fill-rule='evenodd'
-			clip-rule='evenodd'
+			fillRule='evenodd'
+			clipRule='evenodd'
 		></path>
 	</svg>
 )
@@ -351,14 +350,14 @@ const Pin = () => (
 		<path
 			d='M9.62129 1.13607C9.81656 0.940808 10.1331 0.940809 10.3284 1.13607L11.3891 2.19673L12.8033 3.61094L13.8639 4.6716C14.0592 4.86687 14.0592 5.18345 13.8639 5.37871C13.6687 5.57397 13.3521 5.57397 13.1568 5.37871L12.5038 4.7257L8.86727 9.57443L9.97485 10.682C10.1701 10.8773 10.1701 11.1939 9.97485 11.3891C9.77959 11.5844 9.463 11.5844 9.26774 11.3891L7.85353 9.97491L6.79287 8.91425L3.5225 12.1846C3.32724 12.3799 3.01065 12.3799 2.81539 12.1846C2.62013 11.9894 2.62013 11.6728 2.81539 11.4775L6.08576 8.20714L5.0251 7.14648L3.61089 5.73226C3.41563 5.537 3.41562 5.22042 3.61089 5.02516C3.80615 4.8299 4.12273 4.8299 4.31799 5.02516L5.42557 6.13274L10.2743 2.49619L9.62129 1.84318C9.42603 1.64792 9.42603 1.33133 9.62129 1.13607Z'
 			fill='currentColor'
-			fill-rule='evenodd'
-			clip-rule='evenodd'
+			fillRule='evenodd'
+			clipRule='evenodd'
 		></path>
 		<path
 			d='M9.62129 1.13607C9.81656 0.940808 10.1331 0.940809 10.3284 1.13607L11.3891 2.19673L12.8033 3.61094L13.8639 4.6716C14.0592 4.86687 14.0592 5.18345 13.8639 5.37871C13.6687 5.57397 13.3521 5.57397 13.1568 5.37871L12.5038 4.7257L8.86727 9.57443L9.97485 10.682C10.1701 10.8773 10.1701 11.1939 9.97485 11.3891C9.77959 11.5844 9.463 11.5844 9.26774 11.3891L7.85353 9.97491L6.79287 8.91425L3.5225 12.1846C3.32724 12.3799 3.01065 12.3799 2.81539 12.1846C2.62013 11.9894 2.62013 11.6728 2.81539 11.4775L6.08576 8.20714L5.0251 7.14648L3.61089 5.73226C3.41563 5.537 3.41562 5.22042 3.61089 5.02516C3.80615 4.8299 4.12273 4.8299 4.31799 5.02516L5.42557 6.13274L10.2743 2.49619L9.62129 1.84318C9.42603 1.64792 9.42603 1.33133 9.62129 1.13607Z'
 			fill='currentColor'
-			fill-rule='evenodd'
-			clip-rule='evenodd'
+			fillRule='evenodd'
+			clipRule='evenodd'
 		></path>
 	</svg>
 )
@@ -374,8 +373,8 @@ const Trash = () => (
 		<path
 			d='M5.5 1C5.22386 1 5 1.22386 5 1.5C5 1.77614 5.22386 2 5.5 2H9.5C9.77614 2 10 1.77614 10 1.5C10 1.22386 9.77614 1 9.5 1H5.5ZM3 3.5C3 3.22386 3.22386 3 3.5 3H5H10H11.5C11.7761 3 12 3.22386 12 3.5C12 3.77614 11.7761 4 11.5 4H11V12C11 12.5523 10.5523 13 10 13H5C4.44772 13 4 12.5523 4 12V4L3.5 4C3.22386 4 3 3.77614 3 3.5ZM5 4H10V12H5V4Z'
 			fill='currentColor'
-			fill-rule='evenodd'
-			clip-rule='evenodd'
-		></path>
+			fillRule='evenodd'
+			clipRule='evenodd'
+		/>
 	</svg>
 )
--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,35 +1,67 @@
 import axios from 'axios'
 
-export const getShValue = async (
-	stockCodeList: string[]
-): Promise<StockValue[]> => {
+export const getShValue = async (stockCodeList: string[]): Promise<Stock[]> => {
+	if (stockCodeList.length === 0) return []
 	const idListStr = stockCodeList.map((code) => `1.${code}`).join(',')
 	const response = await axios.get(
 		`https://push2.eastmoney.com/api/qt/ulist.np/get?fields=f1,f14,f2,f12,f5,f18,f4&secids=${idListStr}`
 	)
-	const stocks = response.data.data?.diff
-	return stocks?.map((stock: StockValue) => ({ ...stock, type: 'sh' })) || []
+	const stocks = response.data.data?.diff as StockValue[]
+	return (
+		stocks?.map((stock: StockValue) => ({
+			type: 'sh',
+			name: stock.f14,
+			code: stock.f12,
+			current: (stock.f2 / 100).toFixed(2),
+			percent: (stock.f4 * 100) / stock.f18,
+		})) || []
+	)
 }
-export const getSzValue = async (
-	stockCodeList: string[]
-): Promise<StockValue[]> => {
+export const getSzValue = async (stockCodeList: string[]): Promise<Stock[]> => {
+	if (stockCodeList.length === 0) return []
 	const idListStr = stockCodeList.map((code) => `0.${code}`).join(',')
 	const response = await axios.get(
 		`https://push2.eastmoney.com/api/qt/ulist.np/get?fields=f1,f14,f2,f12,f5,f18,f4&secids=${idListStr}`
 	)
-	const stocks = response.data.data?.diff
-	return stocks?.map((stock: StockValue) => ({ ...stock, type: 'sz' })) || []
+	const stocks = response.data.data?.diff as StockValue[]
+	return (
+		stocks?.map((stock: StockValue) => ({
+			type: 'sz',
+			name: stock.f14,
+			code: stock.f12,
+			current: (stock.f2 / 100).toFixed(2),
+			percent: (stock.f4 * 100) / stock.f18,
+		})) || []
+	)
 }
 
 export const getStockValue = async (
 	stockCodeList: string[]
-): Promise<StockValue[]> => {
+): Promise<Stock[]> => {
 	const sh = (await getShValue(stockCodeList)) || []
 	const sz = (await getSzValue(stockCodeList)) || []
-	return [...sh, ...sz]
+	const hk = (await getHkValue(stockCodeList)) || []
+	return [...sh, ...sz, ...hk]
 }
 
-export type StockValue = {
+export const getHkValue = async (stockCodeList: string[]): Promise<Stock[]> => {
+	if (stockCodeList.length === 0) return []
+	const idListStr = stockCodeList.map((code) => `s_hk${code}`).join(',')
+
+	const response = await axios.get(`https://qt.gtimg.cn/q=${idListStr}`)
+	const stocks = convertToStockArray(response.data)
+	return (
+		stocks?.map((stock: Stock) => ({
+			type: 'hk',
+			name: stock.name,
+			code: stock.code,
+			current: stock.current,
+			percent: stock.percent,
+		})) || []
+	)
+}
+
+type StockValue = {
 	f1: number
 	f2: number // 现价
 	f4: number // 股票涨跌
@@ -37,5 +69,35 @@ export type StockValue = {
 	f12: string // 股票代码
 	f14: string // 股票名称
 	f18: number // 开盘价
-	type: 'sh' | 'sz'
+	type: Market
+}
+
+export type Market = 'sh' | 'sz' | 'hk'
+
+export type Stock = {
+	code: string
+	name: string
+	current: string
+	percent: number
+	type: Market
+}
+
+const convertToStockArray = (rawData: string): Stock[] => {
+	return rawData
+		.trim()
+		.split(';')
+		.filter(Boolean)
+		.map((stockData) => {
+			const [, stockInfo] = stockData.split('=')
+			const [, name, code, current, , percent] = stockInfo
+				.replace(/"/g, '')
+				.split('~')
+			return {
+				code,
+				name,
+				current,
+				percent: parseFloat(percent),
+				type: 'hk' as Market,
+			}
+		})
 }
